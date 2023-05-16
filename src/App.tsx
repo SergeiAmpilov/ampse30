0096ad1ea6ef33f7368c8ef5850a73386845a91e
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-05-16 22:29:55 +0300

    feat: remove demo content

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,9 @@ import './App.css';
 import { TestComponent } from './components/components.index'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <TestComponent name='Sergei' />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload and fun.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+    <h1>Hello world</h1>
+  </>);
 }
 
 export default App;
