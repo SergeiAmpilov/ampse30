ac4ee767777951ad7c87e6be0f14ee63cb997223
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-05-17 10:20:39 +0300

    container ready

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,16 @@
 import { FC } from 'react';
 import { HeaderProps } from './Header.props';
-// import '../../assets/font-awesome.min.css';
 
 export const Header: FC<HeaderProps> = ({...props }: HeaderProps): JSX.Element => {
   return (
     <header {...props} >
-      ampilovs.ru
+      <div className="container">
+        ampilovs.ru
+
+        {/* nav menu */}
+        <div>
+        </div>
+      </div>
     </header>
   );
 };
