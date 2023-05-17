09e126f2fb57e3df3160421a8b89c631cd89089a
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-05-17 11:00:09 +0300

    feat: header markdown

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,38 @@
 import { FC } from 'react';
 import { HeaderProps } from './Header.props';
+import styles from './Header.module.css'
+import cn from 'classnames';
 
-export const Header: FC<HeaderProps> = ({...props }: HeaderProps): JSX.Element => {
+export const Header: FC<HeaderProps> = ({className, ...props }: HeaderProps): JSX.Element => {
   return (
-    <header {...props} >
+    <header 
+      className={cn(className, styles.header)}
+      {...props} >
       <div className="container">
-        ampilovs.ru
+
+      <a href="#home">ampilov.s</a>
+
+
+        
+        
 
         {/* nav menu */}
-        <div>
-        </div>
+        <a href="#portfolio">Портфолио</a>
+        <a href="#contacts">Контакты</a>
+
+
+        {/* contacts menu */}
+        <a href="tel: +79268373346" target='_blank'>
+          Phone
+        </a>
+        <a href="mailto:dev@ampilovs.ru" target='_blank'>
+          Email
+        </a>
+        <a href="https://t.me/sergei_ampilov" target='_blank'>
+          Telegramm
+        </a>
+
+
       </div>
     </header>
   );
