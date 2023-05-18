7ef3f81d2a8520adcd3a679803e97f51d2627108
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-05-19 00:02:03 +0300

    fix markdown

--- src/components/Case/Case.tsx
+++ src/components/Case/Case.tsx
@@ -7,7 +7,7 @@ import styles from './Case.module.css'
 export const Case: FC<CaseProps> = ({index, className, ...props}): JSX.Element => {
   return (
     <div
-      className={cn(className)}
+      className={cn(className, styles.case)}
       key={index}
     >
       {props.title}
