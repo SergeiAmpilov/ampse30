9571531d3ffe2e749afd4fcd23b8a4da3f6a1973
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-05-18 23:59:13 +0300

    styles

--- src/components/Case/Case.tsx
+++ src/components/Case/Case.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react';
 import { CaseProps } from './Case.props';
+import cn from 'classnames';
+import styles from './Case.module.css'
+
 
 export const Case: FC<CaseProps> = ({index, className, ...props}): JSX.Element => {
   return (
     <div
+      className={cn(className)}
       key={index}
     >
       {props.title}
