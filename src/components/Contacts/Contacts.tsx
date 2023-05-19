8e42cebb6865e4fee4179c9caad92e8785934388
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-05-19 15:02:14 +0300

    feat: section contacts ready

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -3,10 +3,9 @@ import { FC } from 'react';
 import cn from 'classnames';
 import styles from './Contacts.module.css';
 
-
-
-{/* 
- */}
+import gitHubImg from './github250.png';
+import youtubeImg from './youtube500.png';
+import linkedInnImg from './lin500.png';
 
 
 export const Contacts: FC<ContactsProps>  = ({className, ...props}: ContactsProps):JSX.Element => {
@@ -20,7 +19,7 @@ export const Contacts: FC<ContactsProps>  = ({className, ...props}: ContactsProp
 
         <div className={styles.contacts_container}>
           <div>
-            <h2>Contacts</h2>
+            <h2>Контакты</h2>
             <p>Буду рад пообщаться по поводу вашего проекта:</p>
             <ul className={styles.list}>
               <li>
@@ -43,11 +42,34 @@ export const Contacts: FC<ContactsProps>  = ({className, ...props}: ContactsProp
           <div>
             <h2>Социальные сети</h2>
             <p>Вы можете найти меня в социальных сетях:</p>
-            <ul>
-              <li>GitHub</li>
-              <li>YouTube</li>
-              <li>LinkedInn</li>
-              <li>Codewars</li>
+            <ul className={cn(styles.list, styles.list_blanc)}>
+              <li className={styles.soclink}>
+                <a href="https://github.com/SergeiAmpilov/" className={styles.soclink} target='_blank'>
+                  <img src={gitHubImg} alt="github" className={styles.soclink_img} />
+                </a>
+              </li>
+              <li className={styles.soclink}>
+                <a href="https://www.youtube.com/channel/UCXpXbD_W2W40TEmlahZx7sQ" className={styles.soclink} target='_blank'>
+                  <img src={youtubeImg} alt="Youtube" className={styles.soclink_img} />
+                </a>
+              </li>
+              <li className={styles.soclink}>
+                <a href="https://www.linkedin.com/in/sergei-ampilov/"  target='_blank'>
+                  <img src={linkedInnImg} alt="linked inn" className={styles.soclink_img}/>
+                </a>
+              </li>
+              <li>
+                <img
+                  src="https://www.codewars.com/users/sergei.ampilov/badges/small"
+                  alt="codewars"
+                  className={styles.codewars}
+                   />
+                <img 
+                  src="https://www.codewars.com/users/sergei.ampilov/badges/micro"
+                  alt="codewars"
+                  className={styles.codewars_micro}
+                  />
+              </li>
             </ul>
           </div>
         </div>
--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -2,10 +2,14 @@ import { CASES_DATA } from '../../Data/Cases.data';
 import { Case } from '../components.index';
 import styles from './Portfolio.module.css';
 import { FC } from 'react';
+import cn from 'classnames';
 
-export const Portfolio: FC<any> = ({ ...props }):JSX.Element => {
+
+export const Portfolio: FC<any> = ({ className, ...props }):JSX.Element => {
   return (
-    <section {...props} id='portfolio'>
+    <section
+      className={cn(className, styles.section)}
+      {...props} id='portfolio'>
       <div className="container">
         <h2 className={styles.title}>Portfolio</h2>
         <div className={styles.caselist}>
