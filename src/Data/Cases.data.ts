20624e233ecaf7f15e0cb2d10892d5e3c22fe04d
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-07-03 15:18:41 +0300

    feat: add new cases

--- src/Data/Cases.data.ts
+++ src/Data/Cases.data.ts
@@ -14,7 +14,53 @@ export type Case = {
 
 export const CASES_DATA: Case[]  = [
   {
-    index: 2,
+    index: 10,
+    title: 'Социальная сеть Movie Explorer',
+    img: 'movie.png',
+    description: 'Социальная сеть для поиска фильмов из публичного каталога. После регистрации пользователи могут добалять фильмы в избранное! Также, есть статическая страница-портфолио.',
+    techs: ['HTML', 'CSS', 'Javascript', 'React', 'Node.js', 'Express.js', 'MongoDB'],
+    links: [
+      {
+        name: 'Go LIVE',
+        link: 'https://movie.ampilovs.ru'
+      },
+      {
+        name: 'Api',
+        link: 'https://api.movie.ampilovs.ru'
+      },
+      {
+        name: 'GitHub Frontend',
+        link: 'https://github.com/SergeiAmpilov/movies-explorer-frontend/tree/level-31'
+      },
+      {
+        name: 'GitHub Api backend',
+        link: 'https://github.com/SergeiAmpilov/movies-explorer-api'
+      },
+    ]
+  },
+  {
+    index: 11,
+    title: 'Социальная сеть Mesto',
+    img: 'mesto.png',
+    description: 'Социальная сеть для обмена фотографиями из путешествий с функцией регистрации и авторизации пользователей, размещением постов и возможностью ставить лайки избранным постам!',
+    techs: ['HTML', 'CSS', 'Javascript', 'React', 'Node.js', 'Express.js', 'MongoDB'],
+    links: [
+      {
+        name: 'Go LIVE',
+        link: 'https://mesto.ampilovs.ru'
+      },
+      {
+        name: 'Api',
+        link: 'https://api.mesto.ampilovs.ru'
+      },
+      {
+        name: 'GitHub',
+        link: 'https://github.com/SergeiAmpilov/react-mesto-api-full'
+      },
+    ]
+  },
+  {
+    index: 12,
     title: 'Одностраничный сайт ampilovs.ru',
     img: 'avatar.png',
     description: 'Персональный сайт программиста с портфолио проектов и контактами',
@@ -27,7 +73,7 @@ export const CASES_DATA: Case[]  = [
     ]
   },
   {
-    index: 3,
+    index: 13,
     img: 'weathercli.png',
     title: 'CLI-утилита weather',
     description: 'Утилита командной строки получает прогноз погоды и выводит в консоль',
@@ -44,7 +90,7 @@ export const CASES_DATA: Case[]  = [
     ]
   },
   {
-    index: 4,
+    index: 14,
     img: 'rutravel.jpg',
     title: 'Russian travel',
     description: 'Сайт посвящен путешествиям по Россия',
@@ -62,7 +108,7 @@ export const CASES_DATA: Case[]  = [
   },
 
   {
-    index: 5,
+    index: 15,
     img: 'how-to-learn.jpg',
     title: 'How to learn',
     description: 'Как научиться учиться. Учебный проект на тему методик обучения и эффективности',
